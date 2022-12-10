b793a4674ca99301de56b6a2f72fb47bc44c28c7
Alef oliveira <alefolivfeira@gmail.com>
2022-12-10 16:25:47 -0300

    05_firstStepes

--- src/main/kotlin/br/com/alef/GreetingController.kt
+++ src/main/kotlin/br/com/alef/GreetingController.kt
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
+//teste
 @RestController
 class GreetingController {
 
